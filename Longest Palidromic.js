85b817ac2df49c788debaeab92a41f603c6ad5b3
Jehwa Shin <s.jehwa@gmail.com>
2018-10-03 15:31:19 -0700

    longest Palidromic

--- Longest Palidromic.js
+++ Longest Palidromic.js
@@ -3,22 +3,47 @@
 // Output: "bab"
 // Note: "aba" is also a valid answer.
 
+// let longestPalindrome = (s) => {
+//   let reversed = s.split('').reverse().join('');
+//   let output = '';
+//   let subString = '';
+//   console.log(reversed);
+//   for(let i = 0; i < s.length; i ++) {
+//     if(reversed[i] === s[i]) {
+//       subString += s[i];
+//     } else {
+//       if(subString.length > 1 && subString.length > output.length) {
+//         output = subString;
+//       }
+//       subString = '';
+//     }
+//   }
+//   return output;
+// }
+
 let longestPalindrome = (s) => {
-  let reversed = s.split('').reverse().join('');
-  let output = '';
-  let subString = '';
-  console.log(reversed);
+  let longest = '';
+
+  let findPal = (l,r) => {
+    while(l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++
+    }
+    return s.slice(l+1, r);
+  }
+
   for(let i = 0; i < s.length; i ++) {
-    if(reversed[i] === s[i]) {
-      subString += s[i];
-    } else {
-      if(subString.length > 1 && subString.length > output.length) {
-        output = subString;
-      }
-      subString = '';
+    let oddPal = findPal(i-1, i+1);
+    let evenPal = findPal(i-1, i);
+    if(oddPal.length > longest.length) {
+      longest = oddPal;
+    }
+    if(evenPal.length > longest.length) {
+      longest = evenPal;
     }
   }
-  return output;
+  return longest;
+
 }
 
-console.log(longestPalindrome('abakccdcc'));
+console.log(longestPalindrome('cbbd'));
